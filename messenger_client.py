921ea5dd887b156d8b4479c812e984ca9d6dd921
Aiden Patel <aiden.patel21@gmail.com>
2024-12-09 10:41:58 -0600

    Split encapsulation and decapsulation into separate functions

--- messenger_client.py
+++ messenger_client.py
@@ -10,35 +10,34 @@ class MessengerClient:
         self.client_socket.connect(server_address)
 
 
-    def send_and_receive(self, args, msg):
+    def encapsulate(self, args, msg):
+        ''' Prepare message for sending to server '''
+        encapsulated_msg = "<<<" # add header to the message
+        
+        for arg in args:
+            encapsulated_msg += "$$$" + arg + "$$$" # wrap argument and add it to the message
+
+        encapsulated_msg += msg + ">>>" # add msg and footer to the message
+
+        return encapsulated_msg
+    
+
+    def decapsulate(self, msg):
         return_args   = [] # such as clearing the terminal
         return_msg    = "" # message to print
         return_prompt = "" # prompt for input line
 
-        ''' Prepare message for sending to server '''
-        msg_to_server = "<<<" # add header to the message
-        
-        for arg in args:
-            msg_to_server += "$$$" + arg + "$$$" # wrap argument and add it to the message
-
-        msg_to_server += msg + ">>>" # add msg and footer to the message
-
-        ''' Send and receive '''
-        self.client_socket.send(msg_to_server.encode())
-
-        msg_from_server = (self.client_socket.recv(1024)).decode()
-
         ''' Ensure entire message from the server is received '''
-        if msg_from_server[:3] == "<<<":
+        if msg[:3] == "<<<":
             msg_complete = False
             attempt = 0
             while not(msg_complete):
-                if msg_from_server[-3:] == ">>>":
-                    msg_from_server = msg_from_server[3:-3] # remove header and footer
+                if msg[-3:] == ">>>":
+                    msg = msg[3:-3] # remove header and footer
                     msg_complete = True
 
                 else: # end of message not fully received
-                    msg_from_server += (self.client_socket.recv(1024)).decode()
+                    msg += (self.client_socket.recv(1024)).decode()
                     attempt +=1
 
                 if attempt == 10:
@@ -51,29 +50,42 @@ class MessengerClient:
             return return_args, return_msg, return_prompt
 
         ''' Obtain any arguments given in the message from the server '''
-        num_args = msg_from_server.count("$$$") // 2
+        num_args = msg.count("$$$") // 2
         for i in num_args:
-            if msg_from_server[:3] == "$$$" and msg_from_server[6:9] == "$$$":
-                return_args.append(msg_from_server[3:6])
-                msg_from_server = msg_from_server[9:]
+            if msg[:3] == "$$$" and msg[6:9] == "$$$":
+                return_args.append(msg[3:6])
+                msg = msg[9:]
 
             else: # argument format invalid, '$$$' likely part of message
                 break
 
         ''' Get print out portion of the message from the server '''
-        for i in range(len(msg_from_server)):
-            if msg_from_server[:3] == "|||": # checks if end of print out message
-                msg_from_server = msg_from_server[3:]
+        for i in range(len(msg)):
+            if msg[:3] == "|||": # checks if end of print out message
+                msg = msg[3:]
                 break
             else:
-                return_msg += msg_from_server[0]
-                msg_from_server = msg_from_server[1:]
+                return_msg += msg[0]
+                msg = msg[1:]
 
         ''' Get input prompt portion of the message from the server '''
-        return_prompt = msg_from_server
+        return_prompt = msg
 
         ''' Return argument list, message, and prompt from server '''
         return return_args, return_msg, return_prompt
+
+
+    def send_and_receive(self, args, msg):
+        ''' Encapsulate message '''
+        msg_to_server = self.encapsulate(args, msg)
+
+        ''' Send and receive '''
+        self.client_socket.send(msg_to_server.encode())
+
+        msg_from_server = (self.client_socket.recv(1024)).decode()
+
+        ''' Return decapsulated message '''
+        return self.decapsulate(msg_from_server)
 
 
     def __del__(self):
